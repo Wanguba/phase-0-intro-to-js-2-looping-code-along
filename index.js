5824aedea94998baf9e0b60debd62f10ff167436
Edith Wanguba <wanguamor@gmail.com>
2023-09-23 19:47:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "birthday";
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+        thankYouMessages,push(message);
+    }
+    return thankYouMessages;
+}
+
+const messages = writeCards(names, event);
+console.log(messages);
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown(10);
